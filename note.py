79646c8a7d075e956c20e1e861aa90be54e004a0
David Ward <davidmward@mail.com>
2023-06-11 14:29:08 -0700

    create note class

--- note.py
+++ note.py
@@ -1,6 +1,15 @@
 import sys
 import pickle # For saving and loading objects
 
+class Note():
+    def __init__(self, name):
+        # name is an instance variable
+        self.name = name
+        self.body = ""
+
+    def SetBody(data):
+        self.body = data
+
 def main():
     print()
 
