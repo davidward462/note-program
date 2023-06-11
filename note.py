333cb5064cabee36dbe87509f526bbba93fbc6a6
David Ward <davidmward@mail.com>
2023-06-11 14:31:14 -0700

    add saving function

--- note.py
+++ note.py
@@ -10,6 +10,15 @@ class Note():
     def SetBody(data):
         self.body = data
 
+# Save object to pickle file
+def SaveObject(obj):
+    print("Saving...")
+    try:
+        with open("data.pickle", "wb") as f:
+            pickle.dump(obj, f, protocol=pickle.HIGHEST_PROTOCOL)
+    except Exception as e:
+            print(f"Error occured during pickling object (possibly unsupported): {e}")
+
 def main():
     print()
 
